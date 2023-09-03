7602ee7ab8081d42f860558c5e7b9889550eeb9a
ProgressAce <daiyaansallie7@gmail.com>
2023-09-03 12:54:36 -0700

    add: puppet file to configure ssh config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Puppet manifest to make changes to my ssh client config file
+# - disable need for password authentication
+# - the sought after private key will be found in ~/.ssh
+file_line {'password authenticity':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line {'identitiy key':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh',
+  replace => true,
+}
