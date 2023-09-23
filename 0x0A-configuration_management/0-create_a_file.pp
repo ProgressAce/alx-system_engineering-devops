ebc16015aacb20e2e39071390cfdf26c19508b12
Daiyaan <96121165+ProgressAce@users.noreply.github.com>
2023-09-23 05:33:26 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Uses a puppet manifest to create a file in /tmp
-# the file has 0744 permissions and content placed inside it.
-file {'/tmp/school':
-  ensure  => present,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# Puppet manifest to kill a process named 'killmenow'
-exec {'kill process':
-  command => '/bin/pkill -KILL killmenow'
-}
