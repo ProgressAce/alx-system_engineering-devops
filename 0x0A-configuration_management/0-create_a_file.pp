7df32a38f3be32375785401d2bb40dc5c36f9ed7
ProgressAce <daiyaansallie7@gmail.com>
2023-09-22 21:00:25 -0700

    mod

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp':
   path    => '/tmp/school',
   ensure  => present,
-  mode    => 0744,
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
