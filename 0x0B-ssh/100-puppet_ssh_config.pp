8171c61b02545e33f3e63658a83f42abc89bd34c
ProgressAce <daiyaansallie7@gmail.com>
2023-10-01 13:34:49 -0700

    edit: change ssh config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,8 +3,9 @@ $config_content = 'Host *
     IdentityFile ~/.ssh/school
     PasswordAuthentication no'
 
+# configuring the ssh client config file
 file { 'ssh client config':
   ensure  => 'present',
-  path    => '/root/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   content => $config_content
 }
