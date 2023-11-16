a516b7085d36ec2d73c1cf6780ad136bda63d417
ProgressAce <daiyaansallie7@gmail.com>
2023-11-16 07:00:05 -0800

    add: maifest for fixing server web stack

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Manifest that fixes the server's webstack by allowing nginx to open for files
+
+exec { 'change ulimit -n value':
+  command => "/bin/sed -i 's#ULIMIT=\"-n 15\"#ULIMIT=\"-n 500\"#g' /etc/default/nginx",
+  notify  => Exec["restart nginx"]
+}
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart'
+}
